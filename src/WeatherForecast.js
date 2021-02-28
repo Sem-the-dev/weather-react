fa254b032decb3a34ca9a24daa9f4ac9e0a7da59
sem-the-dev <semhar.tesfu@outlook.com>
2021-02-28 22:54:46 +0000

    Add forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,16 +1,95 @@
 import React, {useState} from "react";
 import axios from 'axios';
+import ReactAnimatedWeather from "react-animated-weather";
 
 export default function WeatherForecast(props){
-  const [loaded, setLoaded] = useState(false)
-    function getForecast(response){
-      console.log(response.data)
-  }
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
+
+  function getForecast(response){
+    setForecast(response.data)
+    setLoaded(true);
+  };
   
-  
+  if (loaded) {
+    return (
+<div className="row hourly-weather WeatherForecast">
+<div className="col">
+{new Date(forecast.list[0].dt * 1000).getHours()}:00
+<br/>
+  <ReactAnimatedWeather
+    icon="CLEAR_DAY"
+    color="gray"
+    size={50}
+    animate={true}
+  />
+<br/>
+H:{Math.round(forecast.list[0].main.temp_max)}° 
+L:{Math.round(forecast.list[0].main.temp_min)}°
+</div> 
+<div className="col">
+{new Date(forecast.list[1].dt * 1000).getHours()}:00
+<br/>
+  <ReactAnimatedWeather
+    icon="CLEAR_DAY"
+    color="gray"
+    size={50}
+    animate={true}
+  />
+<br/>
+H:{Math.round(forecast.list[1].main.temp_max)}° 
+L:{Math.round(forecast.list[1].main.temp_min)}°
+
+</div> 
+<div className="col">
+{new Date(forecast.list[2].dt * 1000).getHours()}:00
+<br/>
+  <ReactAnimatedWeather
+    icon="CLEAR_DAY"
+    color="gray"
+    size={50}
+    animate={true}
+  />
+<br/>
+H:{Math.round(forecast.list[2].main.temp_max)}° 
+L:{Math.round(forecast.list[2].main.temp_min)}°
+
+</div> 
+<div className="col">
+{new Date(forecast.list[3].dt * 1000).getHours()}:00
+<br/>
+  <ReactAnimatedWeather
+    icon="CLEAR_DAY"
+    color="gray"
+    size={50}
+    animate={true}
+  />
+<br/>
+H:{Math.round(forecast.list[3].main.temp_max)}° 
+L:{Math.round(forecast.list[3].main.temp_min)}°
+
+</div> 
+<div className="col">
+{new Date(forecast.list[4].dt * 1000).getHours()}:00
+<br/>
+  <ReactAnimatedWeather
+    icon="CLEAR_DAY"
+    color="gray"
+    size={50}
+    animate={true}
+  />
+<br/>
+H:{Math.round(forecast.list[4].main.temp_max)}° 
+L:{Math.round(forecast.list[4].main.temp_min)}°
+
+</div> 
+</div>     
+)
+  } else {
   let apiKey = `aea55e6fcec85bd9372335be0d239f49`;
   let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${props.city}&units=metric&appid=${apiKey}`;
   axios.get(forecastUrl).then(getForecast);
     
-    return props.city
+    return null;
+}
 }
