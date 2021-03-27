482a71d5e84b498a807e89062bae6867c8270b4b
sem-the-dev <semhar.tesfu@outlook.com>
2021-03-27 11:59:38 +0000

    Fix descendent bug

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -18,7 +18,7 @@ export default function FormattedDate(props){
     if (hours < 10) {hours = `0${hours}`}
   let minutes = props.date.getMinutes();
     if (minutes < 10) {minutes = `0${minutes}`};
-  return( <p> {day} {hours}:{minutes}</p>);
+  return( `${day} ${hours}:${minutes}`);
 
 
 }
